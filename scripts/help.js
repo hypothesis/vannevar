c70feecb99563896e0213cf3a20188be8df3f5d3
Nick Stenning <nick@whiteink.com>
2015-03-02 17:45:56 +0000

    Provide our own help module...
    ...which doesn't spam the channel.

---
+++ scripts/help.js
@@ -0,0 +1,40 @@
+// Description:
+//   Generates help commands for Hubot.
+//
+// Commands:
+//   hubot help - Displays all of the help commands that Hubot knows about.
+//   hubot help <query> - Displays all help commands that match <query>.
+//
+// Notes:
+//   These commands are grabbed from comment blocks at the top of each file.
+//
+module.exports = function (robot) {
+    robot.respond(/help\s*(.*)?$/i, function (msg) {
+        var cmds = robot.helpCommands();
+        var filter = msg.match[1];
+
+        if (filter) {
+            cmds = cmds.filter(function (cmd) {
+                return cmd.match(new RegExp(filter, 'i'));
+            });
+            if (cmds.length === 0) {
+                msg.send("No available commands match " + filter);
+                return;
+            }
+        }
+
+        prefix = robot.alias || robot.name;
+        cmds = cmds.map(function (cmd) {
+            cmd = cmd.replace(/hubot/ig, robot.name);
+            return cmd.replace(new RegExp("^" + robot.name), prefix);
+        });
+
+        emit = cmds.join("\n");
+
+        if (typeof msg.sendPrivate === 'function') {
+            msg.sendPrivate(emit);
+        } else {
+            msg.send(emit);
+        }
+    });
+};
